9ea7ca229b8169d21a9e9f06bdadb43c85de3403
ElianeMariadeAndrade <ElianeMariadeAndrade@BRA174>
2025-01-16 16:46:38 -0300

    Primeiro commit

---
+++ WebCredentialsApi/Services/PermissionService.cs
@@ -0,0 +1,18 @@
+﻿namespace WebCredentialsApi.Service;
+
+public class PermissionService
+{
+    private readonly string _senha;
+
+    public PermissionService(string senha)
+    {
+        _senha = senha;
+    }
+
+    public bool ValidarSenha(string senhaUsuario)
+    {
+        return senhaUsuario == _senha;
+    }
+}
+
+
